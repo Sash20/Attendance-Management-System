5d59eb9e1586ab93a22d392c8380357cbe90f3a2
Keneel <tkeneel@gmail.com>
2023-12-01 08:36:04 -0500

    Remove unnecessary code

--- mysql/course.sql
+++ mysql/course.sql
@@ -11,20 +11,3 @@ CREATE TABLE `course_info` (
     `end_date` VARCHAR(255) DEFAULT NULL
 );
 
-DROP TABLE IF EXISTS `course_info`;
-CREATE TABLE `students_detail` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `first_name` VARCHAR(50) NOT NULL,
-  `last_name` VARCHAR(50) NOT NULL,
-  `date_of_birth` DATE NOT NULL,
-  `courses` VARCHAR(255) UNIQUE NOT NULL
-);
-
-DROP TABLE IF EXISTS `student_courses`;
-CREATE TABLE `student_courses` (
-  `student_id` INT UNSIGNED NOT NULL,
-  `course_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`student_id`, `course_id`),
-  FOREIGN KEY (`student_id`) REFERENCES `students_detail` (`id`) ON DELETE CASCADE,
-  FOREIGN KEY (`course_id`) REFERENCES `course_info` (`id`) ON DELETE CASCADE
-);
